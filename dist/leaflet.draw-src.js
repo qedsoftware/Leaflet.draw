aa5820f79529e08ba5bebb34c86944a548a1113e
Michał Łazowik <m1@qed.ai>
2018-10-24 14:32:28 +0200

    Build

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1,5 +1,5 @@
 /*
- Leaflet.draw 1.0.3, a plugin that adds drawing and editing tools to Leaflet powered maps.
+ Leaflet.draw 1.0.3+5954e63, a plugin that adds drawing and editing tools to Leaflet powered maps.
  (c) 2012-2017, Jacob Toye, Jon West, Smartrak, Leaflet
 
  https://github.com/Leaflet/Leaflet.draw
@@ -8,7 +8,7 @@
 (function (window, document, undefined) {/**
  * Leaflet.draw assumes that you have already included the Leaflet library.
  */
-L.drawVersion = "1.0.3";
+L.drawVersion = "1.0.3+5954e63";
 /**
  * @class L.Draw
  * @aka Draw
@@ -1712,7 +1712,9 @@ L.Edit.Marker = L.Handler.extend({
 	addHooks: function () {
 		var marker = this._marker;
 
-		marker.dragging.enable();
+		if (marker.dragging != null) {
+			marker.dragging.enable();
+		}
 		marker.on('dragend', this._onDragEnd, marker);
 		this._toggleMarkerHighlight();
 	},
@@ -1722,7 +1724,9 @@ L.Edit.Marker = L.Handler.extend({
 	removeHooks: function () {
 		var marker = this._marker;
 
-		marker.dragging.disable();
+		if (marker.dragging != null) {
+			marker.dragging.disable();
+		}
 		marker.off('dragend', this._onDragEnd, marker);
 		this._toggleMarkerHighlight();
 	},
